a7face02c409c93b27299f763682715e54966967
kainthsourav <souravkainth@outlook.com>
2020-04-22 18:08:55 +0530

    Routing Basic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,10 @@ import {MatButtonModule} from "@angular/material/button";
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import {AppRoutingModule} from "../app/app-routing/app-routing.module";
 
 @NgModule({
   declarations: [
@@ -22,7 +26,11 @@ import { FooterComponent } from './footer/footer.component';
     MenuComponent,
     DishdetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent,
+    
   ],
   imports: [
     BrowserModule,
@@ -32,7 +40,8 @@ import { FooterComponent } from './footer/footer.component';
     MatListModule,
     MatGridListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
