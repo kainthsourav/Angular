9de22a82529504d500d04151928dff77a97f6d4e
kainthsourav <souravkainth@outlook.com>
2020-04-24 01:22:50 +0530

    Reactive Form and Validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,10 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {FormsModule} from '@angular/forms';
+import {ReactiveFormsModule} from '@angular/forms';
+import{MatSelectModule} from '@angular/material/select';
+import{ MatSlideToggleModule} from '@angular/material/slide-toggle';
+
 
 //Import Services here
 import{DishService} from '../app/Services/dish.service';
@@ -57,7 +61,11 @@ import { LoginComponent } from './login/login.component';
     MatFormFieldModule,
     MatInputModule,
     MatCheckboxModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatSlideToggleModule
+
   ],
   providers: [
     DishService,
